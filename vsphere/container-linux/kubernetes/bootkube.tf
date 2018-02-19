14f5676f345a945f089045a414398e1b898ac983
bkcsfi <bkc@sfi.ca>
2018-02-18 19:38:44 -0500

    Change etcd_servers to use domain name, not ip_address

--- vsphere/container-linux/kubernetes/bootkube.tf
+++ vsphere/container-linux/kubernetes/bootkube.tf
@@ -4,12 +4,11 @@ module "bootkube" {
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${var.k8s_domain_name}"]
-  etcd_servers          = ["${var.controller_ip_addresses}"]
+  etcd_servers          = ["${var.k8s_domain_name}"]
   asset_dir             = "${var.asset_dir}"
   networking            = "${var.networking}"
   network_mtu           = "${var.network_mtu}"
   pod_cidr              = "${var.pod_cidr}"
   service_cidr          = "${var.service_cidr}"
   cluster_domain_suffix = "${var.cluster_domain_suffix}"
-
-}
+  }
