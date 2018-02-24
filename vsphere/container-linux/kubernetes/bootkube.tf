1cee5553d6e4b630fc58ccb953e7566dd3456de9
bkcsfi <bkc@sfi.ca>
2018-02-24 11:52:17 -0500

    works w/ 3 controller setup

--- vsphere/container-linux/kubernetes/bootkube.tf
+++ vsphere/container-linux/kubernetes/bootkube.tf
@@ -3,7 +3,7 @@ module "bootkube" {
   source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=070d1846448252acc8ebe6d40aaa555e65b69b85"
 
   cluster_name          = "${var.cluster_name}"
-  api_servers           = ["${null_resource.repeat.*.triggers.domain}"]
+  api_servers           = ["${var.k8s_domain_name}"]
   etcd_servers          = ["${null_resource.repeat.*.triggers.domain}"]
   asset_dir             = "${var.asset_dir}"
   networking            = "${var.networking}"
